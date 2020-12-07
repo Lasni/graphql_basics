2aa4b64ec715ae84585d19d00380578c97741421
grega <grega.lasnibat@gmail.com>
2020-12-07 14:11:43 +0100

    querying with arguments

--- src/index.js
+++ src/index.js
@@ -2,31 +2,102 @@ import {
   GraphQLServer
 } from "graphql-yoga"
 
+// demo data array
+const users = [{
+  id: '1',
+  name: 'Grega',
+  email: 'grega@gmail.com',
+  age: 32
+}, {
+  id: '2',
+  name: 'Mujko',
+  email: 'mujko@gmail.com'
+}, {
+  id: '3',
+  name: 'Scungilli Man',
+  email: 'scungilli@gmail.com'
+}]
+
+const posts = [{
+    id: 'postid1',
+    title: 'postTitle1',
+    body: 'postBody1',
+    published: false
+  },
+  {
+    id: 'postid2',
+    title: 'postTitle2',
+    body: 'postBody2',
+    published: true
+  },
+  {
+    id: 'postid3',
+    title: 'postTitle3',
+    body: 'postBody3',
+    published: false
+  }
+]
 
 // Type definitions (schema)
 const typeDefs = `
   type Query {
-    hello: String!
+    me: User!
+    post: Post!
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
+  }
+
+  type User {
+    id: ID!
     name: String!
-    location: String!
-    bio: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return "This is my first query"
+    me() {
+      return {
+        id: 'userID',
+        name: 'Grega',
+        email: 'grega@gmail.com',
+        age: 32
+      }
     },
-    name() {
-      return "Grega Lasnibat"
+    post() {
+      return {
+        id: 'postID',
+        title: 'Title post',
+        body: 'This is some post body',
+        published: true
+      }
     },
-    location() {
-      return "Ljubljana, Slovenija"
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users
+      }
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
+      })
     },
-    bio() {
-      return "Keeping my pants high and tight, following proto and feathering it."
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts
+      }
+      return posts.filter((post) => {
+        const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+        const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+        return isTitleMatch || isBodyMatch
+      })
     }
   }
 }
